02e0537a1b5de90fb6b9b5a91a9f0c3df2cc4671
isaacmogaka <isaacmogaka2016@gmail.com>
2023-06-12 12:01:52 +0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+let submitBtn = document.getElementById("submit")
+
+submitBtn.addEventListener('click', function(){
+    let name = document.getElementById('Name').value ()
+    let email = document.getElementById("Email").value ()
+    let contacts = document.getElementById("Contacts").value ()
+    console.log(name)
+    console.log(email)
+    console.log(contacts)
+
+})
